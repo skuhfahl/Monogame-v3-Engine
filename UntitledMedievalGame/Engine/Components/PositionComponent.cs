cee4f29a2064f93d243ef40568c0be84383eea75
Steven <steven.kuhfahl@gmail.com>
2020-03-04 23:04:15 -0500

    Position component creating for defining the position, dimensions, and rotation of an entity

---
+++ UntitledMedievalGame/Engine/Components/PositionComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace UntitledMedievalGame.Engine.Components
+{
+    /// <summary>
+    /// Responsible for holding a Coordinate Vector2 (X, Y), Dimensions Vector2 (X, Y),
+    /// and a Rotation float.
+    /// </summary>
+    class PositionComponent
+    {
+        public Vector2 Position { get; set; }
+        public Vector2 Dimensions { get; set; }
+        public float Rotation { get; set; }
+    }
+}

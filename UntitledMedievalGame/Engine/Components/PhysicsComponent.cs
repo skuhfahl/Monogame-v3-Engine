009b60cf06d432736d7e669a165f68c309b4b10b
Steven <steven.kuhfahl@gmail.com>
2020-03-05 23:07:30 -0500

    Created a Phyiscs component for applying forces to entity.

---
+++ UntitledMedievalGame/Engine/Components/PhysicsComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace UntitledMedievalGame.Engine.Components
+{
+    /// <summary>
+    /// The Physics Component is responsible for applying forces to the entity. 
+    /// </summary>
+    class PhysicsComponent
+    {
+        public Vector2 Velocity;
+    }
+}

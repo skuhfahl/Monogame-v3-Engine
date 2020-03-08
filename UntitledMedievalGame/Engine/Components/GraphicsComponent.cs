607cc1b2b6e7206ea8d683a6a50dce37851a29af
Steven <steven.kuhfahl@gmail.com>
2020-03-07 21:34:19 -0500

    Implement basic rendering system that is responsible for drawing sprite to the screen

--- UntitledMedievalGame/Engine/Components/GraphicsComponent.cs
+++ UntitledMedievalGame/Engine/Components/GraphicsComponent.cs
@@ -11,54 +11,12 @@ namespace UntitledMedievalGame.Engine.Components
     /// </summary>
     class GraphicsComponent : IComponent
     {
-        //Services
-        private ContentManager contentManager = GameServices.GetService<ContentManager>();
-        private SpriteBatch spriteBatch = GameServices.GetService<SpriteBatch>();
-
-        //Public Fields
-
-        //Private Fields
-        private Vector2 dimensions;
-        private Texture2D sprite;
-        private SpriteEffects spriteEffects;
+        public Texture2D sprite;
+        public SpriteEffects spriteEffects;
 
         public GraphicsComponent()
         {
 
-        }
-
-        public void Update(Entity entity)
-        {
-            // Load the sprite corresponding to the entity type
-            string spritePath = GetSpritePath(entity.type);
-            sprite = contentManager.Load<Texture2D>(spritePath);
-        }
-
-        private string GetSpritePath(EntityType type)
-        {
-            //To do - Don't want to have to manually add a new case for each sprite.
-
-            string spritePath;
-
-            switch (type)
-            {
-                case EntityType.PlayerPlaceholder:
-                    spritePath = "PlayerPlaceholder";
-                    break;
-                case EntityType.Rat:
-                    spritePath = "Rat";
-                    break;
-                default:
-                    spritePath = "TextureError";
-                    break;
-            }
-
-            return spritePath;
-        }
-
-        public void Draw(Entity entity)
-        {
-            spriteBatch.Draw(sprite, new Rectangle((int)entity.position.X, (int)entity.position.Y, (int)entity.dimensions.X, (int)entity.dimensions.Y), null, Color.White, 0.0f, new Vector2(sprite.Bounds.Width / 2, sprite.Bounds.Height / 2), spriteEffects, 0.0f);
         }
     }
 }
---
+++ UntitledMedievalGame/Engine/Systems/RenderingSystem.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UntitledMedievalGame.Engine.Components;
+using UntitledMedievalGame.Engine.Entities;
+using UntitledMedievalGame.Engine.Services;
+
+namespace UntitledMedievalGame.Engine.Systems
+{
+    class RenderingSystem
+    {
+        //Services
+        private ContentManager contentManager = GameServices.GetService<ContentManager>();
+        private SpriteBatch spriteBatch = GameServices.GetService<SpriteBatch>();
+        
+        List<Entity> entities;
+
+        public RenderingSystem(List<Entity> entities)
+        {
+            this.entities = entities;
+        }
+        
+        public void Update()
+        {
+            foreach(Entity e in entities)
+            {
+                GraphicsComponent gc = e.GetComponent<GraphicsComponent>();
+                PositionComponent pc = e.GetComponent<PositionComponent>();
+
+                if (gc != null && pc != null)
+                {
+                    string spritePath = GetSpritePath(e.type);
+                    gc.sprite = contentManager.Load<Texture2D>(spritePath);
+                    spriteBatch.Draw(
+                        gc.sprite, 
+                        new Rectangle((int)pc.Position.X, (int)pc.Position.Y, (int)pc.Dimensions.X, (int)pc.Dimensions.Y), 
+                        null, 
+                        Color.White, 
+                        pc.Rotation, 
+                        new Vector2(gc.sprite.Bounds.Width / 2, gc.sprite.Bounds.Height / 2), gc.spriteEffects, 0.0f
+                        );
+                }
+            }
+        }
+
+        private string GetSpritePath(EntityType type)
+        {
+            string spritePath;
+
+            switch (type)
+            {
+                case EntityType.PlayerPlaceholder:
+                    spritePath = "PlayerPlaceholder";
+                    break;
+                case EntityType.Rat:
+                    spritePath = "Rat";
+                    break;
+                default:
+                    spritePath = "TextureError";
+                    break;
+            }
+
+            return spritePath;
+        }
+
+    }
+}
